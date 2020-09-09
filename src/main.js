dbfc5cb6d68743ad099f87a0a924d3d25aef33e7
castletco <diegocastillonaranjo@gmail.com>
2020-09-09 12:35:17 -0300

    configuración básica con firebase para un proyecto Vue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueFirestore from 'vue-firestore'
+import { db } from './firebase'
+import firebase from 'firebase'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(VueFirestore, { key: 'id' })
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+console.log(db)
+firebase.auth().onAuthStateChanged(user => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
---
+++ src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Registro
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style scoped>
+
+</style>
